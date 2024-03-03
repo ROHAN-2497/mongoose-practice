e4444c47f345e32116c749d113177c70aa54cf89
ROHAN-2497 <rohanislam3669@gmail.com>
2024-03-04 00:04:47 +0600

    up

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+const mongoose = require("mongoose");
+
+// database connection
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/mongoose-practice");
+    console.log(`database connection succesful`);
+  } catch (err) {
+    console.log(`failed to database connect`, err);
+  }
+}
+main();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`server is listening on port ${port}`);
 });
